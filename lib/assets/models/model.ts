8258cdfa9c488f9cde6f00a59dfc73b2441c0131
Dominic Rico Gómez <hello@coderocket.me>
2022-03-16 13:10:53 +0000

    bump version

--- lib/assets/models/model.ts
+++ lib/assets/models/model.ts
@@ -48,8 +48,8 @@ interface <%= name[0].toUpperCase() + name.substr(1) %>Attributes {
   updatedAt?: Date
 }
 
-export interface <%= name[0].toUpperCase() + name.substr(1) %>Input extends Optional<<%= name[0].toUpperCase() + name.substr(1) %>Attributes, 'id'> {}
-export interface <%= name[0].toUpperCase() + name.substr(1) %>Output extends Required<<%= name[0].toUpperCase() + name.substr(1) %>Attributes> {}
+export interface <%= name[0].toUpperCase() + name.substr(1) %>Input extends Optional< <%= name[0].toUpperCase() + name.substr(1) %>Attributes, 'id' > {}
+export interface <%= name[0].toUpperCase() + name.substr(1) %>Output extends Required< <%= name[0].toUpperCase() + name.substr(1) %>Attributes > {}
 
 class <%= name[0].toUpperCase() + name.substr(1) %> extends Model {
   public id!: number
