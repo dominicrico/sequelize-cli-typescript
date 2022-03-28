9f4a4bc58cb7a33c31d316047292e643be44ce6a
Dominic Rico-Gomez <hello@coderocket.me>
2022-03-28 13:00:08 +0200

    Update create-table.ts

--- src/assets/migrations/create-table.ts
+++ src/assets/migrations/create-table.ts
@@ -1,34 +1,40 @@
-import { QueryInterface, DataTypes } from 'sequelize'
+import {
+    DataTypes
+} from 'sequelize'
+//@ts-ignore
+import { umzug } from '../bin/commands/db.js'
 
-export = {
-    up: (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
-        return queryInterface.createTable('<%= tableName %>', {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER
-            },
+export type Migration = typeof umzug._types.migration
 
-        <% attributes.forEach(function (attribute) { %>
-            <%= attribute.fieldName %>: {
-                type: Sequelize.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(Sequelize.${attribute.dataType.toUpperCase()})` : attribute.dataType.toUpperCase() %>
-            },
-        <% }) %>
+export const up: Migration = async ({ context: queryInterface }) => {
+    await queryInterface.createTable('<%= tableName %>', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
 
-            <%= createdAt %>: {
-                allowNull: false,
-                type: Sequelize.DATE
-            },
+    <% attributes.forEach(function (attribute) { %>
+        <%= attribute.fieldName %>: {
+            type: Sequelize.<%= attribute.dataFunction ? `${attribute.dataFunction.toUpperCase()}(Sequelize.${attribute.dataType.toUpperCase()})` : attribute.dataType.toUpperCase() %>
+        },
+    <% }) %>
 
-            <%= updatedAt %>: {
-                allowNull: false,
-                type: Sequelize.DATE
-            }
-        })
-    },
+        <%= createdAt %>: {
+            allowNull: false,
+            type: Sequelize.DATE
+        },
 
-    down: (queryInterface: QueryInterface, Sequelize: typeof DataTypes) => {
-        return queryInterface.dropTable('<%= tableName %>')
-    }
+        <%= updatedAt %>: {
+            allowNull: false,
+            type: Sequelize.DATE
+        }
+    })
 }
+    
+//@ts-ignore
+export const down: Migration = async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('<%= tableName %>')
+}
+
